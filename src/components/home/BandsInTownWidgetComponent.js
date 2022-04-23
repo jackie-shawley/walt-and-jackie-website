a64a4b64efd54c4bb7a801ea73a9935c673fd11b
jackie-shawley <jackiekopco@aol.com>
2022-04-23 18:15:39 -0400

    favicon and title

---
+++ src/components/home/BandsInTownWidgetComponent.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+
+
+function BandsInTownWidget() {
+
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        const api = https:rest.bandsintown.com/artists/{{Walt & Jackie}}/?app_id=yOUrSuP3r3ven7aPp-id
+    })
+    return(
+
+    );
+}
